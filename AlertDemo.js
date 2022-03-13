a488d107c0b70f1b9146194a40562beb77b753da
gittcoder <adityaavinash.n2020@vitstudent.ac.in>
2022-03-13 12:14:08 +0530

    FirstCommit

--- AlertDemo.js
+++ AlertDemo.js
@@ -48,7 +48,7 @@ export default function CustomStyles() {
     <ThemeProvider theme={theme}>
       <Box
       sx={{height:"10vh"}}>
-      <StyledAlert variant="d" color="error" severity="Warning" sx={{
+      <StyledAlert variant="d" color="error" severity="warning" sx={{
         width: "50vw",
         height: "5vh",
         border:"2px solid #DCDCDC",
